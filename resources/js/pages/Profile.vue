82c207b41b99941c9bb262908017b473e939f287
dityawbo <dityahwbo@gmail.com>
2025-05-10 18:04:29 +0700

    commit

---
+++ resources/js/pages/Profile.vue
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<template>
+    PROFILE PAGE
+</template>
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,24 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return Inertia::render('Home');
 })->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('skema', function () {
+    return Inertia::render('Scheme');
+})->name('skema');
+
+Route::get('profil', function () {
+    return Inertia::render('Profile');
+})->name('profil');
+
+Route::get('pendaftaran', function () {
+    return Inertia::render('Pendaftaran');
+})->name('pendaftaran');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
